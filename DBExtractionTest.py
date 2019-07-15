6b9fba8dd0e6ce33f3abc52d94d9bdff907c2b06
aperrien@razordisk.com <aperrien@gmail.com>
2019-07-14 22:16:08 -0700

    Added a test for the beginnings of the web interface

---
+++ DBExtractionTest.py
@@ -0,0 +1,49 @@
+import sqlite3
+import datetime
+import yaml
+import json
+
+def nest(rows):
+    root = {}
+    for row in rows:
+        d = root
+        for item in row[:-2]:
+            d = d.setdefault(item, {})
+        d[row[-2]] = row[-1]
+    return root
+
+
+# Open a submission, store it's comments in a nested dictionary
+
+stream = open(r"PowerBeeConfig.yaml", 'r')
+try:
+    PBconfig = yaml.safe_load(stream)
+except yaml.YAMLError as exc:
+    print(exc)
+
+downloaderConfig = PBconfig['downloader_config']
+
+connection = sqlite3.connect(r"RedditArchive.db")
+cursor = connection.cursor()
+
+
+
+statement = """
+SELECT 
+       SUBSTR(ParentID,4) as ParentID,
+       CommentID,
+       CommentBody,
+       datetime(CreationTime,'unixepoch') as Creation_Time 
+    FROM comments
+WHERE LinkID = 't3_3z2yhb'
+"""
+
+cursor.execute(statement)
+
+results = cursor.fetchall()
+
+commentList = nest(results)
+
+
+
+print(json.dumps(commentList, sort_keys=True, indent=4))
